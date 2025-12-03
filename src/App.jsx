33c5138aae9075022b3df172fb393b604213c77d
Gerald Baalham <geraldbaalham@live.co.uk>
2025-12-03 14:15:21 +0000

    Add Contact window component with user connection details and social links

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Resume from "./windows/Resume";
 import Finder from "./windows/Finder";
 import Text from "./windows/Text";
 import Image from "./windows/Image";
+import Contact from "./windows/Contact";
 
 gsap.registerPlugin(Draggable);
 
@@ -22,6 +23,7 @@ const App = () => {
       <Finder/>
       <Text />
       <Image/>
+      <Contact/>
     </main>
   )
 }
