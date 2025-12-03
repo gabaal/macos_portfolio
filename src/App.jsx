e50d8008f974cade80495875771b5fd533dd2d63
Gerald Baalham <geraldbaalham@live.co.uk>
2025-12-03 11:52:17 +0000

    Add Image and Text window components with content display and window controls

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Terminal from "./windows/Terminal";
 import Safari from "./windows/Safari";
 import Resume from "./windows/Resume";
 import Finder from "./windows/Finder";
+import Text from "./windows/Text";
+import Image from "./windows/Image";
 
 gsap.registerPlugin(Draggable);
 
@@ -18,6 +20,8 @@ const App = () => {
       <Safari/>
       <Resume/>
       <Finder/>
+      <Text />
+      <Image/>
     </main>
   )
 }
